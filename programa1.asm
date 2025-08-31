761910478d50caf810ce616b410e8ace8e9a3f03
Karen <158315598+nogueiiiiiiiiira@users.noreply.github.com>
2025-08-31 12:35:21 -0300

    Update programa1.asm

--- programa1.asm
+++ programa1.asm
@@ -1,4 +1,4 @@
- .data
+.data
  	num1 : .word  1
  	num2 : .word  2
  	num3 : .word  3
@@ -7,32 +7,34 @@
  	num6 : .word  9
  	linha: .asciiz "\n"
  	virgula: .asciiz ", "
- 	cabecalho1: .asciiz "PROGRAMA 1: ORDEM CRESCENTE"
+ 	cabecalho1: .asciiz "PROGRAMA 1: ORDEM CRESCENTE (sem array)"
+ 	t_nao_ordenados: .asciiz "´Números Não Ordenados: "
+ 	t_ordenados: .asciiz "´Números Ordenados: "
 
  .text 
  
  	li $v0, 4
- 	la $a0, cabecalho
+ 	la $a0, cabecalho1
  	syscall 
  	
  	li $v0, 4
  	la $a0, linha
  	syscall 
  	
-	li $v0, 1   
-	lw $a0, num1
+ 	li $v0, 4
+ 	la $a0, linha
+ 	syscall 
+ 	
+ 	li $v0, 4
+ 	la $a0, t_nao_ordenados
+ 	syscall 
+ 	
+ 	li $v0, 1   
+	lw $a0, num4
 	syscall 
 	
 	li $v0, 4
-    	la $a0, virgula
-	syscall
-	
-	li $v0, 1   
-	lw $a0, num2
-	syscall
-	
-	li $v0, 4
-    	la $a0, virgula
+    la $a0, virgula
 	syscall
 	
 	li $v0, 1   
@@ -40,15 +42,15 @@
 	syscall
 	
 	li $v0, 4
-    	la $a0, virgula
+    la $a0, virgula
 	syscall
 	
 	li $v0, 1   
-	lw $a0, num4
+	lw $a0, num6
 	syscall
 	
 	li $v0, 4
-    	la $a0, virgula
+    la $a0, virgula
 	syscall
 	
 	li $v0, 1   
@@ -56,7 +58,71 @@
 	syscall
 	
 	li $v0, 4
-    	la $a0, virgula
+    la $a0, virgula
+	syscall
+	
+	li $v0, 1   
+	lw $a0, num2
+	syscall
+	
+	li $v0, 4
+    la $a0, virgula
+	syscall
+	
+	li $v0, 1   
+	lw $a0, num1
+ 	syscall       
+ 	
+	li $v0, 4
+ 	la $a0, linha
+ 	syscall    
+ 	
+ 	li $v0, 4
+ 	la $a0, linha
+ 	syscall 
+ 	
+ 	li $v0, 4
+ 	la $a0, t_nao_ordenados
+ 	syscall 
+ 	
+	li $v0, 1   
+	lw $a0, num1
+	syscall 
+	
+	li $v0, 4
+    la $a0, virgula
+	syscall
+	
+	li $v0, 1   
+	lw $a0, num2
+	syscall
+	
+	li $v0, 4
+    la $a0, virgula
+	syscall
+	
+	li $v0, 1   
+	lw $a0, num3
+	syscall
+	
+	li $v0, 4
+    la $a0, virgula
+	syscall
+	
+	li $v0, 1   
+	lw $a0, num4
+	syscall
+	
+	li $v0, 4
+    la $a0, virgula
+	syscall
+	
+	li $v0, 1   
+	lw $a0, num5
+	syscall
+	
+	li $v0, 4
+    la $a0, virgula
 	syscall
 	
 	li $v0, 1   
