479fc9b41bd41731aa8bb6b5a16226e3f968788f
Karen <158315598+nogueiiiiiiiiira@users.noreply.github.com>
2025-08-31 12:40:53 -0300

    Update programa1.asm

--- programa1.asm
+++ programa1.asm
@@ -7,7 +7,7 @@
  	num6 : .word  9
  	linha: .asciiz "\n"
  	virgula: .asciiz ", "
- 	cabecalho1: .asciiz "PROGRAMA 1: ORDEM CRESCENTE (sem array)"
+ 	cabecalho1: .asciiz "PROGRAMA 1: ORDEM CRESCENTE (sem array e sem if/else)"
  	t_nao_ordenados: .asciiz "´Números Não Ordenados: "
  	t_ordenados: .asciiz "´Números Ordenados: "
 
