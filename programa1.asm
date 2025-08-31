d91697979a9ae8c9cf9b023f3eafbd5225db3a65
Karen <158315598+nogueiiiiiiiiira@users.noreply.github.com>
2025-08-31 12:10:57 -0300

    Create programa1.asm

---
+++ programa1.asm
@@ -0,0 +1,64 @@
+ .data
+ 	num1 : .word  1
+ 	num2 : .word  2
+ 	num3 : .word  3
+ 	num4 : .word  4
+ 	num5 : .word  5
+ 	num6 : .word  9
+ 	linha: .asciiz "\n"
+ 	virgula: .asciiz ", "
+ 	cabecalho1: .asciiz "PROGRAMA 1: ORDEM CRESCENTE"
+
+ .text 
+ 
+ 	li $v0, 4
+ 	la $a0, cabecalho
+ 	syscall 
+ 	
+ 	li $v0, 4
+ 	la $a0, linha
+ 	syscall 
+ 	
+	li $v0, 1   
+	lw $a0, num1
+	syscall 
+	
+	li $v0, 4
+    	la $a0, virgula
+	syscall
+	
+	li $v0, 1   
+	lw $a0, num2
+	syscall
+	
+	li $v0, 4
+    	la $a0, virgula
+	syscall
+	
+	li $v0, 1   
+	lw $a0, num3
+	syscall
+	
+	li $v0, 4
+    	la $a0, virgula
+	syscall
+	
+	li $v0, 1   
+	lw $a0, num4
+	syscall
+	
+	li $v0, 4
+    	la $a0, virgula
+	syscall
+	
+	li $v0, 1   
+	lw $a0, num5
+	syscall
+	
+	li $v0, 4
+    	la $a0, virgula
+	syscall
+	
+	li $v0, 1   
+	lw $a0, num6
+ 	syscall          
