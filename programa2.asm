70731b374ab1a364ec04479d14a11a4b256851bc
Karen <158315598+nogueiiiiiiiiira@users.noreply.github.com>
2025-08-31 16:15:36 -0300

    Update programa2.asm

--- programa2.asm
+++ programa2.asm
@@ -1,11 +1,10 @@
  .data
  	data_nasc1 : .word 2006
  	data_nasc2 : .word  2025
- 	linha: .asciiz "\n"
  	virgula: .asciiz ", "
- 	t_data_nasc1 : .asciiz "Data de Nascimento 1: 2006"
- 	t_data_nasc2 : .asciiz "Data de Nascimento 2: 2025"
- 	resultado: .asciiz "RESULTADO SOMA: "
+ 	t_data_nasc1 : .asciiz "\nData de Nascimento 1: 2006"
+ 	t_data_nasc2 : .asciiz "\nData de Nascimento 2: 2025"
+ 	resultado: .asciiz "\nRESULTADO SOMA: "
  	cabecalho2: .asciiz "PROGRAMA 2: SOMA DOS NASCIMENTOS"
 
  .text 
@@ -15,32 +14,12 @@
  	syscall 
  	
  	li $v0, 4
- 	la $a0, linha
- 	syscall 
- 	
- 	li $v0, 4
- 	la $a0, linha
- 	syscall 
- 	
- 	li $v0, 4
  	la $a0, t_data_nasc1
  	syscall
  	
  	li $v0, 4
-	la $a0, linha
-	syscall 
- 	
- 	li $v0, 4
  	la $a0, t_data_nasc2
  	syscall
- 	
- 	li $v0, 4
-	la $a0, linha
-	syscall 
-	
-	li $v0, 4
- 	la $a0, linha
- 	syscall 
 	
 	li $v0, 4
 	la $a0, resultado
