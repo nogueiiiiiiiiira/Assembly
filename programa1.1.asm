46334d94a6c8b6a7eee5b5eea1636410847f0d93
Karen <158315598+nogueiiiiiiiiira@users.noreply.github.com>
2025-09-04 20:28:17 -0300

    Update programa1.1.asm

--- programa1.1.asm
+++ programa1.1.asm
@@ -4,19 +4,64 @@
     t_nao_ordenado: .asciiz "\nNão ordenado: "
     t_ordenado:   .asciiz "\nOrdenado: "
     virgula: .asciiz ", "
-
 .text
     main:
-    
-        li $s0, 0 # contador
-        la $t7, sequencia
-        
+        li $s0, 0           
+        la $t7, sequencia    
+
         lw $t0, 0($t7)             
         lw $t1, 4($t7)         
         lw $t2, 8($t7)              
         lw $t3, 12($t7)            
         lw $t4, 16($t7)        
         lw $t5, 20($t7)         
+        
+        li $v0, 4
+        la $a0, cabecalho
+        syscall
+
+        li $v0, 4
+        la $a0, t_nao_ordenado
+        syscall
+
+        li $v0, 1
+        move $a0, $t0
+        syscall
+        li $v0, 4
+        la $a0, virgula
+        syscall
+
+        li $v0, 1
+        move $a0, $t1
+        syscall
+        li $v0, 4
+        la $a0, virgula
+        syscall
+
+        li $v0, 1
+        move $a0, $t2
+        syscall
+        li $v0, 4
+        la $a0, virgula
+        syscall
+
+        li $v0, 1
+        move $a0, $t3
+        syscall
+        li $v0, 4
+        la $a0, virgula
+        syscall
+
+        li $v0, 1
+        move $a0, $t4
+        syscall
+        li $v0, 4
+        la $a0, virgula
+        syscall
+
+        li $v0, 1
+        move $a0, $t5
+        syscall
 
     compara_t0_t1:
         ble $t0, $t1, compara_t1_t2
@@ -43,62 +88,17 @@
         move $t6, $t4
         move $t4, $t5
         move $t5, $t6
-        
+
     garantindo_ordenar:
         addi $s0, $s0, 1
         blt $s0, 5, compara_t0_t1
 
-        li $v0, 4
-        la $a0, cabecalho
-        syscall
-
-        li $v0, 4
-        la $a0, t_nao_ordenado
-        syscall
-
-        li $v0, 1
-        lw $a0, 0($t7)
-        syscall
-
-        li $v0, 4
-        la $a0, virgula
-        syscall
-
-        li $v0, 1
-        lw $a0, 4($t7)
-        syscall
-
-        li $v0, 4
-        la $a0, virgula
-        syscall
-
-        li $v0, 1
-        lw $a0, 8($t7)
-        syscall
-
-        li $v0, 4
-        la $a0, virgula
-        syscall
-
-        li $v0, 1
-        lw $a0, 12($t7)
-        syscall
-
-        li $v0, 4
-        la $a0, virgula
-        syscall
-
-        li $v0, 1
-        lw $a0, 16($t7)
-        syscall
-
-        li $v0, 4
-        la $a0, virgula
-        syscall
-
-        li $v0, 1
-        lw $a0, 20($t7)
-        syscall
+        sw $t0, 0($t7)
+        sw $t1, 4($t7)
+        sw $t2, 8($t7)
+        sw $t3, 12($t7)
+        sw $t4, 16($t7)
+        sw $t5, 20($t7)
 
         li $v0, 4
         la $a0, t_ordenado
@@ -107,7 +107,7 @@
         li $v0, 1
         move $a0, $t0
         syscall
-
+        
         li $v0, 4
         la $a0, virgula
         syscall
@@ -115,7 +115,6 @@
         li $v0, 1
         move $a0, $t1
         syscall
-
         li $v0, 4
         la $a0, virgula
         syscall
@@ -123,7 +122,7 @@
         li $v0, 1
         move $a0, $t2
         syscall
-
+        
         li $v0, 4
         la $a0, virgula
         syscall
@@ -131,7 +130,7 @@
         li $v0, 1
         move $a0, $t3
         syscall
-
+        
         li $v0, 4
         la $a0, virgula
         syscall
@@ -139,7 +138,6 @@
         li $v0, 1
         move $a0, $t4
         syscall
-
         li $v0, 4
         la $a0, virgula
         syscall
